bb4e51093e350125b95ea01e00f4238da1c8662b
André Rennó <andrerenno00@gmail.com>
2018-06-29 23:08:58 -0300

    Made many updates most of the functions, including the removal of the "voidTile" terminated array in exchange for a NULL terminated array, which is much simpler.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
 #include <stdlib.h>
-#include <stdio.h>
 
-#define MAXHAND 40
+#include <stdio.h>
+#include <curses.h>
+
+#define MAXHAND 106
 #define NUMOFTILES 106
-				    	       
+
 
 typedef struct Tile {
-	unsigned char number;
-	unsigned char suit;
+    unsigned char number;
+    unsigned char suit;
 } Tile;
 
 void tile_swap(Tile *a1, Tile *a2){
-	Tile temp = *a1;
-	*a1 = *a2;
-	*a2 = temp;
+    Tile temp = *a1;
+    *a1 = *a2;
+    *a2 = temp;
 }
 
 void tile_sort(Tile* arr[]){
 
     int size;
-    for (size = 0; arr[size] -> number != 255; size++); 
+    for (size = 0; arr[size] != NULL; size++); 
     for (int i = 0; i < size; i++)
         for (int j = 0; i < size - 1; j++)
             if (arr[j] -> number > arr[j + 1] -> number)
@@ -28,90 +30,106 @@ void tile_sort(Tile* arr[]){
 
 void fisher_yates(Tile* arr[]){        // Fisher Yates shuffle
 
-	int randint;
+    int randint;
     int size = 0;
-    for (size = 0; (arr[size] -> number) != 255; size++);
-	for (int i = (size - 1); i > 0; i--){
-		randint = rand() % (i + 1);	
-		tile_swap(arr[i], arr[randint]);
-	} 
+    for (size = 0; arr[size] != NULL; size++);
+    for (int i = (size - 1); i > 0; i--){
+        randint = rand() % (i + 1);	
+        tile_swap(arr[i], arr[randint]);
+    } 
 }
 void pool_get(Tile *hand[], Tile* pool[]){
 
-    Tile* voidTile;
-    voidTile = malloc(sizeof(Tile));
-    voidTile -> number = 255;
-    voidTile -> suit = 255;
-
     int i, j;
     i = j = 0;
-    
-    /* Find the last tile on the hand (a void tile) */
-    for (j = 0; hand[j] -> number != voidTile -> number; j++);
 
-    (hand[j+1]) = voidTile;
-    tile_swap(hand[j], pool[i]);
+    /* Find the last element on the array  (a NULL pointer) */
+    for (j = 0; hand[j] != NULL; j++);
 
-    /* pool[i] is now a void tile */
-    /* this next loop brings the void tile to the end of the pool */ 
-    while ((pool[i + 1] -> number) != (voidTile -> number)){
-        tile_swap(pool[i], pool [i+1]);
+    hand[j+1] = NULL;
+    hand[j] = pool[i];
+
+    /* this next loop brings the tile that the player got to the end of the array (after the NULL pointer) */ 
+    while (pool[i-1] != NULL ){
+        pool[i] = pool[i + 1];
         i++;
     } 
 
-    free(pool[i+1]);
 }
 
-void arr_put(Tile* hand_tile, Tile* arr[]){
-    
-    
+void arr_put(Tile* hand[], unsigned int index, Tile* arr[]){
+
+    int i;
+    for(i = 0; arr[i] != NULL; i++);
+    arr[i + 1] = NULL;
+    arr[i] = hand[index];
+
+    while (hand[index-1] != NULL ){
+        hand[index] = hand[index + 1];
+        index++;
+    } 
+
 
 }
 
 int main(int argc, char *argv[]){
+	/* curses initializations. */
+	initscr();
+	noecho();
+	keypad(stdscr, TRUE);
+	nodelay(stdscr, FALSE);
+	curs_set(0);
+	start_color();
+	init_pair(1, COLOR_MAGENTA, COLOR_BLACK);	//Used for the "Quarto" in the title screen.
+	init_pair(2, COLOR_BLUE, COLOR_BLACK);  	//Used for player 1's turn.
+	init_pair(3, COLOR_GREEN, COLOR_BLACK);		//Used for player 2's turn.
+	init_pair(4, COLOR_WHITE, COLOR_RED);		//Used for error messages.
+	init_pair(5, COLOR_CYAN, COLOR_BLACK);		//Used for the X's that replace numbers that where already used. 
 
-    Tile* voidTile = malloc(sizeof(Tile));
-    voidTile -> number = 255;
-    voidTile -> suit = 255;
 
-	Tile** pool = malloc((NUMOFTILES + 2) * sizeof(Tile*));
+
+    Tile** pool = malloc((NUMOFTILES + 2) * sizeof(Tile*));
     if (pool == NULL)
         return 1;
 
-	for (int i = 0; i < (NUMOFTILES - 2); i++){
+    for (int i = 0; i < (NUMOFTILES - 2); i++){
         pool[i] = malloc(sizeof(Tile));
-		pool[i] -> number = (i % 13) + 1;
-		pool[i] -> suit = (i / 13) % 4;
-	}
+        pool[i] -> number = (i % 13) + 1;
+        pool[i] -> suit = (i / 13) % 4;
+    }
 
-	pool[NUMOFTILES - 1] = malloc(sizeof(Tile));
-	pool[NUMOFTILES - 1] -> number = 14;
-	pool[NUMOFTILES - 1] -> suit = 2;
+    pool[NUMOFTILES - 1] = malloc(sizeof(Tile));
+    pool[NUMOFTILES - 1] -> number = 14;
+    pool[NUMOFTILES - 1] -> suit = 2;
 
-	pool[NUMOFTILES - 2] = malloc(sizeof(Tile));
-	pool[NUMOFTILES - 2] -> number = 14;
-	pool[NUMOFTILES - 2] -> suit = 1;
+    pool[NUMOFTILES - 2] = malloc(sizeof(Tile));
+    pool[NUMOFTILES - 2] -> number = 14;
+    pool[NUMOFTILES - 2] -> suit = 1;
 
-    pool[NUMOFTILES] = voidTile;
-	
-	Tile** players[2];
-    players[0] = malloc(MAXHAND * sizeof(Tile));
-    players[1] = malloc(MAXHAND * sizeof(Tile));
+    pool[NUMOFTILES] = NULL;
 
-    players[0][0] = voidTile;
-    players[1][0] = voidTile;
+    Tile** player[2];
+    player[0] = malloc(MAXHAND * sizeof(Tile));
+    player[1] = malloc(MAXHAND * sizeof(Tile));
 
-	fisher_yates(pool);
+    player[0][0] = NULL;
+    player[1][0] = NULL;
+
+    fisher_yates(pool);
 
     Tile table[NUMOFTILES];
 
+    int c = 0;
+    while ((c = getch()) != '\n'){
+        pool_get(player[0], pool);
 
-    for (int i = 0; pool[i] -> number != voidTile -> number; i++){
-        printf("Num %d, Suit: %d\n", pool[i] -> number, pool[i] -> suit);
+        clear();
+        printw("Player Hand\n");
+        for (int i = 0; player[0][i] != NULL ; i++){
+            printw("Num %d, Suit: %d\n", player[0][i] -> number, player[0][i] -> suit);
+        }
     }
-    
-    for (int i = 0; i < 14; i++){
-        
-    }
+
+    endwin();
 
 }
